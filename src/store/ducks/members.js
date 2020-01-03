3c80b6d85fb383492b9c442e24061c9c121d7605
André Luís Batista Kunde <andrelbkunde@gmail.com>
2020-01-03 15:16:27 -0300

    Listing members

--- src/store/ducks/members.js
+++ src/store/ducks/members.js
@@ -5,6 +5,8 @@ import Immutable from 'seamless-immutable';
 const { Types, Creators } = createActions({
   openMembersModal: null,
   closeMembersModal: null,
+  getMembersRequest: null,
+  getMembersSuccess: ['data'],
 });
 
 export const MembersTypes = Types;
@@ -13,6 +15,7 @@ export default Creators;
 /* Initial State */
 
 const INITIAL_STATE = Immutable({
+  data: [],
   membersModalOpen: false,
 });
 
@@ -22,9 +25,12 @@ export const openModal = state => state.merge({ membersModalOpen: true });
 
 export const closeModal = state => state.merge({ membersModalOpen: false });
 
+export const getSuccess = (state, { data }) => state.merge({ data });
+
 /* Reducers to types */
 
 export const reducer = createReducer(INITIAL_STATE, {
   [Types.OPEN_MEMBERS_MODAL]: openModal,
   [Types.CLOSE_MEMBERS_MODAL]: closeModal,
+  [Types.GET_MEMBERS_SUCCESS]: getSuccess,
 });
